601b7d563e0c842fec820294ffc10c83ef9479b7
HenriqueSouza4 <riqueplays11@gmail.com>
2021-04-20 20:25:17 -0300

    Serviços incluir, editar, remover aluno

--- academifybackend/src/main/java/com/br/academify2/resource/AlunoResource.java
+++ academifybackend/src/main/java/com/br/academify2/resource/AlunoResource.java
@@ -4,10 +4,9 @@ package com.br.academify2.resource;
 import com.br.academify2.model.Aluno;
 import com.br.academify2.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,7 +21,30 @@ public class AlunoResource {
 
     @GetMapping("/listar")
     public List<Aluno> listar() {
+
         return alunoRepository.findAll();
+    }
+
+    @GetMapping("get/{id}")
+    public Aluno get(@PathVariable(value = "id") long id){
+        return alunoRepository.findById(id);
+    }
+
+    @PostMapping("/incluir")
+    public Aluno incluir(@RequestBody Aluno aluno) {
+        return alunoRepository.save(aluno);
+    }
+
+    @PutMapping("/editar")
+    public ResponseEntity<Aluno> editar(@RequestBody Aluno aluno){
+        aluno =  alunoRepository.save(aluno);
+        return new ResponseEntity<Aluno>(aluno, HttpStatus.OK);
+
+    }
+    @PostMapping("/remover")
+    public Aluno remover(@RequestBody Aluno aluno) {
+        alunoRepository.delete(aluno);
+        return aluno;
     }
 
 
